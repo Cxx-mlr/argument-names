46ac257c514fdc09eb3e2438caaa05ad754140a7
Cxx-mlr <spapiernik12@gmail.com>
2024-09-23 19:31:15 -0500

    Add basic test case for sum function
    - Added test_sum() with a basic assertion to ensure tests are running.

---
+++ tests/test_argument_names.py
@@ -0,0 +1,8 @@
+from argument_names import argument_names
+
+@argument_names(function=lambda a, b: print(f"{a=}, {b=}"))
+def sum(a: int, b: int) -> int:
+    return a + b
+
+def test_a():
+    assert True
